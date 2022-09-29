26918849e2bfbcdcd45b3369b15ad97e314c4663
Michel Marinho <michel.marinho04@outlook.com>
2022-09-29 11:35:24 -0400

    Add: Titulo Component

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,23 @@
 <script lang="ts">
-	export let name: string;
+	import Titulo from "./components/Titulo.svelte";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<div class="app">
+	<header>
+		<Titulo />
+	</header>
+</div>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+	.app {
+		max-height: 100vh;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+	header {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+
 </style>
